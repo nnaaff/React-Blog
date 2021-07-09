4b686b97330316faa55369ba789067cdeab29734
unknown <nafisee@gmail.com>
2021-07-10 02:14:31 +0530

    Filtered props

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
   const [articles, setArticles] = useState( [
     { title: 'My New Blog', body: 'lorem ipsum...', author: 'A', id: 1 },
     { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'B', id: 2 },
-    { title: 'Wellness Tips', body: 'lorem ipsum...', author: 'C', id: 3 }
+    { title: 'Wellness Tips', body: 'lorem ipsum...', author: 'A', id: 3 }
   ] );
 
   return (
     <div className="home">
-      <ArtList articles={ articles } title="List of Articles:" /> 
+      <ArtList articles={ articles } title="List of Articles:" />
+      <ArtList articles={ articles.filter( (article) => article.author === 'A' ) } title="A's Articles:" />
     </div>
   );
 }
