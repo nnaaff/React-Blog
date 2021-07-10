641d70d8ab64cc3b2129a1d47f202b74b81bb03e
unknown <nafisee@gmail.com>
2021-07-11 02:04:01 +0530

    Added loading message

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -4,22 +4,23 @@ import ArtList from './ArtList';
 const Home = () => {
 
   const [articles, setArticles] = useState( null );
-
-  const [name, setName] = useState( 'A' );
+  const [isPending, setIsPending] = useState( true );
 
   useEffect( () => {
-    fetch( "http://localhost:8000/articles" ) 
+    fetch( "http://localhost:8000/articles" )
       .then( (res) => {
         return res.json();
       } )
       .then( (data) => {
         // console.log( data );
         setArticles( data );
+        setIsPending( false );
       } );
   }, [] );
 
   return (
     <div className="home">
+      { isPending && <div> Loading... </div> }
       { articles && < ArtList articles={ articles } title="Article List:" /> }
     </div>
   );
