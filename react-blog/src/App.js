24f00918d81fec1ef59445cc4eab20c4f5d5ab2a
unknown <nafisee@gmail.com>
2021-07-12 02:02:18 +0530

    React router dom

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,6 +1,8 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 // Component: a function that returns a JSX template & is exported at the bottom of the file
 function App() {
@@ -8,12 +10,23 @@ function App() {
   // any valid JavaScript here - before returning the component
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
